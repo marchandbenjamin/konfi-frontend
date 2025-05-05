b12cc2fc2d26fd264be3e8002094d9e7f59c1a96
Benjamin Marchand <bmarchand@ippon.fr>
2025-05-05 16:39:57 +0200

    feat: centred pages content

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 import Recipes from './components/Recipes';
 import RecipeDetail from './components/RecipeDetail';
+import { Box } from '@chakra-ui/react';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipes" element={<Recipes />} />
-        <Route path="/recipes/:id" element={<RecipeDetail />} />
-      </Routes>
+      <Box 
+      display="flex" 
+      justifyContent="center" 
+      alignItems="center" 
+      minHeight="100vh"
+      flexDirection="column"
+      p={4}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/recipes/:id" element={<RecipeDetail />} />
+        </Routes>
+      </Box>
     </Router>
   );
 }
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { Box, Heading, Button, List, ListItem } from '@chakra-ui/react';
+import { Box, Heading, Button, List, ListItem, Flex } from '@chakra-ui/react';
 import { getAllRecipes } from '../services/recipeService.js';
 
 function Recipes() {
@@ -15,29 +15,40 @@ function Recipes() {
   }, []);
 
   return (
-    <Box p={6}>
-      <Heading mb={4}>Liste des recettes</Heading>
-      <List>
-        {recipes.length > 0 ? (
-          recipes.map((recipe) => (
-            <ListItem key={recipe.id}>
-              <Link to={`/recipes/${recipe.id}`}>
-                <Button variant="link" colorScheme="teal">
-                  {recipe.title}
-                </Button>
-              </Link>
-            </ListItem>
-          ))
-        ) : (
-          <ListItem>Aucune recette disponible.</ListItem>
-        )}
-      </List>
-      <Link to="/">
-        <Button colorScheme="teal" mt={4}>
-          Retour à l'accueil
-        </Button>
-      </Link>
-    </Box>
+    <Flex 
+    justify="center" 
+    align="center" 
+    minH="100vh" 
+    w="100%"
+    p={6}>
+      <Box maxW="600px" w="100%">
+        <Heading mb={4} textAlign="center">
+          Liste des recettes
+        </Heading>
+        <List spacing={3}>
+          {recipes.length > 0 ? (
+            recipes.map((recipe) => (
+              <ListItem key={recipe.id} textAlign="center">
+                <Link to={`/recipes/${recipe.id}`}>
+                  <Button variant="link" colorScheme="teal">
+                    {recipe.title}
+                  </Button>
+                </Link>
+              </ListItem>
+            ))
+          ) : (
+            <ListItem textAlign="center">Aucune recette disponible.</ListItem>
+          )}
+        </List>
+        <Flex justify="center" mt={6}>
+          <Link to="/">
+            <Button colorScheme="teal">
+              Retour à l'accueil
+            </Button>
+          </Link>
+        </Flex>
+      </Box>
+    </Flex>
   );
 }
 
